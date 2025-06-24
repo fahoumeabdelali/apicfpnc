4e9f134725fcd30caf2c869ab939a2c0ca5b16e3
fahoumeabdelali <abdelalifahoume@gmail.com>
2025-06-24 19:47:58 +0100

    Update DbContext.js

--- DbContext.js
+++ DbContext.js
@@ -14,7 +14,8 @@ const dbpassword = process.env.DB_PASS
 /*** Connexion à la base de données */
 let sequelize = new Sequelize( databasename, dbuser, dbpassword, { 
     host: databasehost, 
-    port: databaseport,dialect: 'mysql',
+    port: databaseport,
+    dialect: 'mysql',
     // desactivation de l'affichage dans la console des requettes sql générées par sequelize quand il s'agit de l'environement de production .
     logging:  console.log
     // logging: process.env.NODE_ENV === 'production' ? false : console.log
