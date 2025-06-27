caf7357d3f5b1bf79a0ccd39b064e577bd629935
fahoumeabdelali <abdelalifahoume@gmail.com>
2025-06-27 15:49:46 +0100

    Update UserRoles.js

--- models/UserRoles.js
+++ models/UserRoles.js
@@ -1,7 +1,3 @@
-/************************************/
-/*** Import des modules nécessaires */
-const { DataTypes } = require('sequelize')
-
 module.exports = (sequelize) => {
     const UserRoles = sequelize.define('UserRoles', {}, { timestamps: false })
     return UserRoles
